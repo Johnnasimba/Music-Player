40dea25c0bcc3621996ca0822a6421f0f3da827c
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-06 23:06:04 +0200

    Assign the progress bar width to progress percent

--- script.js
+++ script.js
@@ -96,7 +96,10 @@ loadSong(songs[songIndex]);
 // Update Progress Bar & Time
 function updateProgressBar(e) {
   if (isPlaying) {
-    console.log(e)
+    const { duration, currentTime } = e.srcElement;
+    // Update the progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
   }
 }
 
